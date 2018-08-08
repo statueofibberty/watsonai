01105702a2445787b42b4e32fab499a4c73aa875
Ibrahim M Syed <IbrahimSyed@Ibrahims-MacBook-Pro-3.local>
2018-08-08 16:44:57 -0400

    done

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
